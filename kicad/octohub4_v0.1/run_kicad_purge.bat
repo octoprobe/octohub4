2d8fd1198afdbcd373fb2e62813aa791c265d817
Hans Märki <buhtig.hans.maerki@ergoinfo.ch>
2026-01-14 12:14:09 +0100

    run_kicad_purge.bat

--- kicad/octohub4_v0.1/run_kicad_purge.bat
+++ kicad/octohub4_v0.1/run_kicad_purge.bat
@@ -1,7 +1,7 @@
-# Run kicad_purge on current directory
-#
-# NO files will be changed.
-# All purging has to be manually!
+@rem Run kicad_purge on current directory
+
+@rem NO files will be changed.
+@rem All purging has to be manually!
 
 uv run --with=git+https://github.com/hmaerki/experiment_kicad_purge@main kicad_purge
 
